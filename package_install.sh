23d2277495d5642641d12e3b44bd3a81aee57c05
DanVanAtta <dhvatta@gmail.com>
2016-02-20 19:55:45 -0800

    Add travis/ruby dependencies to apt-get install

--- package_install.sh
+++ package_install.sh
@@ -35,8 +35,9 @@ sudo apt-get update
 sudo apt-get install git eclipse meld sublime-text-installer google-chrome-stable ntp ntpdate lm-sensors psensor -y
 sudo apt-get install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller -y
 sudo apt-get install cairo-dock cairo-dock-plug-ins ubuntu-after-install -y
-sudo apt-get install parallel expect compizconfig-settings-manager -y
+sudo apt-get install parallel expect ruby ruby-dev compizconfig-settings-manager -y
 
+sudo gem install travis -v 1.8.2 --no-rdoc --no-ri
 
 
 service apparmor stop 
